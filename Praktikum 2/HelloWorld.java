eb40f8d5046f696a0db645003fc7473f83ffa9b2
mdaffaaryandaru <daffaaryandaru100815@gmail.com>
2020-09-20 19:54:40 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println ("Hello Daffa!") ;
+		System.out.println ("Nama:Muhammad Daffa Aryandaru") ;
+		System.out.println ("Nim:6706204080") ;
+		System.out.println ("Kelas:D3IF4401") ;
+	}
+}
